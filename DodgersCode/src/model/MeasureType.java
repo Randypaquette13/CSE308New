9c4faf15c48bc20b0b4973ecd16621b7332e3a1c
Randypaquette13 <Randypaquette13@gmail.com>
2019-04-18 19:39:21 -0400

    added MeasureType

---
+++ DodgersCode/src/model/MeasureType.java
@@ -0,0 +1,49 @@
+package model;
+
+public enum MeasureType {
+
+    SCHWARTZBERG_COMPACTNESS {
+        @Override
+        public double calculateMeasure() {
+            return -1.0;//TODO
+        }
+    },
+    REOCK_COMPACTNESS {
+        @Override
+        public double calculateMeasure() {
+            return -1.0;//TODO
+        }
+    },
+    POLSBY_POPPER_COMPACTNESS {
+        @Override
+        public double calculateMeasure(District D) {
+            return -1.0;//TODO
+        }
+    },
+    CONVEX_HULL_COMPACTNESS {
+        @Override
+        public double calculateMeasure(District D) {
+            return -1.0;//TODO
+        }
+    },
+    EFFICIENCY_GAP {
+        @Override
+        public double calculateMeasure(District D) {
+            return -1.0;//TODO
+        }
+    },
+    PARTISAIN_FAIRNESS {
+        @Override
+        public double calculateMeasure(District D) {
+            return -1.0;//TODO
+        }
+    },
+    RACIAL_FAIRNESS {
+        @Override
+        public double calculateMeasure(District D) {
+            return -1.0;//TODO
+        }
+    };
+
+    public abstract double calculateMeasure(District D);
+}
