911897f062bc1e3598d8cdd928637423f1fc3040
ChristopherGiaramita <christopher.giaramita@stonybrook.edu>
2019-04-19 17:33:29 -0400

    got server working

--- DodgersCode/src/server/src/main/java/cse308/server/ServerApplication.java
+++ DodgersCode/src/server/src/main/java/cse308/server/ServerApplication.java
@@ -2,10 +2,18 @@ package cse308.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration; //remove this when DB is configured
 
-@SpringBootApplication
+/**
+ * The class containing the main method used to start the server.
+ */
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class}) //remove the exclude when the DB is configured
 public class ServerApplication {
 
+    /**
+     * The main method used to start the Spring server
+     * @param args arguments
+     */
     public static void main(String[] args) {
         SpringApplication.run(ServerApplication.class, args);
     }
--- DodgersCode/src/server/src/main/java/cse308/server/TestGreeting.java
+++ DodgersCode/src/server/src/main/java/cse308/server/TestGreeting.java
@@ -1,19 +1,30 @@
 package cse308.server;
 
-import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * This class is to be used as a reference for how other controller classes should look.
+ */
 @RestController
 public class TestGreeting {
 
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
+    private static final String template = "Hello, %s! ";
 
-    @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(),
-                String.format(template, name));
+    /**
+     * A test controller mapped to "/greeting". This is to be used as a reference for other controllers if need be.
+     *
+     * url format: localhost:8080/greeting ? name = ______ & message = _______
+     *
+     * @param name A name in the query string of the url. If not is specified, will default to "World"
+     * @param msg Some message in the query string of the url. If not is specified, will default to "No message
+     *            specified"
+     * @return A string of the format "Hello, 'name'! 'msg'"
+     */
+    @RequestMapping("/greeting")    //replace "/greeting" with the endpoint
+    public String greeting(@RequestParam(value = "name", defaultValue = "World") String name,
+                           @RequestParam(value = "message", defaultValue = "No message specified") String msg) {
+        return String.format(template, name) + msg;
     }
 }
