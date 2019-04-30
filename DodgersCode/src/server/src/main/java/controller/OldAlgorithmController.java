69c20c9a89fa4c00814cb29404c63e2c2a330c46
Randypaquette13 <Randypaquette13@gmail.com>
2019-04-30 18:33:57 -0400

    unfucked the old algorithm controller

---
+++ DodgersCode/src/server/src/main/java/controller/OldAlgorithmController.java
@@ -0,0 +1,30 @@
+package controller;
+
+import model.Preference;
+import model.State;
+import model.Summary;
+
+import java.util.Collection;
+import java.util.LinkedList;
+
+@Deprecated
+public class OldAlgorithmController {
+
+    Collection<Preference> prefs;
+    State s;
+
+    public OldAlgorithmController(Collection<Preference> prefs, String state) {//TODO do we even need this? maybe make all static
+        this.prefs = prefs;
+        this.s = null;//EntityManager.loadState(state);//TODO EntityManager needs to be made
+    }
+
+    public Collection<Summary> startJobs() {
+        Collection<Summary> summaryBatch = new LinkedList<>();
+
+        for(Preference p : prefs) {
+            s.reset(p);
+            summaryBatch.add(new Algorithm(p,s).doJob());//TODO is this all we need algorithm for?
+        }
+        return summaryBatch;
+    }
+}
