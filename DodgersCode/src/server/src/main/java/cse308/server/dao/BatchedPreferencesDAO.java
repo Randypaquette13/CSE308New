692b3b2d131784868a942810261ff0fd2f2205b7
Randypaquette13 <Randypaquette13@gmail.com>
2019-05-15 21:29:40 -0400

    batched prefs

---
+++ DodgersCode/src/server/src/main/java/cse308/server/dao/BatchedPreferencesDAO.java
@@ -0,0 +1,249 @@
+package cse308.server.dao;
+
+import model.MeasureType;
+import model.Preference;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+public class BatchedPreferencesDAO {
+    private double schwartzbergCompactnessMax;
+    private double schwartzbergCompactnessMin;
+    private double reackCompactnessMax;
+    private double reackCompactnessMin;
+    private double populationEqualityMax;
+    private double populationEqualityMin;
+    private double polsbyCompactnessMax;
+    private double polsbyCompactnessMin;
+    private double convexCompactnessMax;
+    private double convexCompactnessMin;
+    private double graphCompactnessMax;
+    private double graphCompactnessMin;
+    private double efficiencyGapMax;
+    private double efficiencyGapMin;
+    private boolean graphPartUpdate;
+    private int maxMajMinDistricts;
+    private int minMajMinDistricts;
+    private int numDistricts;
+    private String stateName;
+
+    public BatchedPreferencesDAO(double schwartzbergCompactnessMax, double schwartzbergCompactnessMin,
+                               double reackCompactnessMax, double reackCompactnessMin, double populationEqualityMax,
+                               double populationEqualityMin, double polsbyCompactnessMax, double polsbyCompactnessMin,
+                               double convexCompactnessMax, double convexCompactnessMin, double graphCompactnessMax,
+                               double graphCompactnessMin, double efficiencyGapMax, double efficiencyGapMin,
+                               boolean graphPartUpdate, int maxMajMinDistricts, int minMajMinDistricts,
+                               int numDistricts, String stateName) {
+        this.schwartzbergCompactnessMax = schwartzbergCompactnessMax;
+        this.schwartzbergCompactnessMin = schwartzbergCompactnessMin;
+        this.reackCompactnessMax = reackCompactnessMax;
+        this.reackCompactnessMin = reackCompactnessMin;
+        this.populationEqualityMax = populationEqualityMax;
+        this.populationEqualityMin = populationEqualityMin;
+        this.polsbyCompactnessMax = polsbyCompactnessMax;
+        this.polsbyCompactnessMin = polsbyCompactnessMin;
+        this.convexCompactnessMax = convexCompactnessMax;
+        this.convexCompactnessMin = convexCompactnessMin;
+        this.graphCompactnessMax = graphCompactnessMax;
+        this.graphCompactnessMin = graphCompactnessMin;
+        this.efficiencyGapMax = efficiencyGapMax;
+        this.efficiencyGapMin = efficiencyGapMin;
+        this.graphPartUpdate = graphPartUpdate;
+        this.maxMajMinDistricts = maxMajMinDistricts;
+        this.minMajMinDistricts = minMajMinDistricts;
+        this.numDistricts = numDistricts;
+        this.stateName = stateName;
+    }
+
+    public List<Preference> makePreferences(){
+        LinkedList<Preference> preferences = new LinkedList<>();
+
+        for(int i=0; i < numDistricts; i++ ) {
+            HashMap<MeasureType, Double> hashmap = new HashMap<MeasureType, Double>();
+            for(MeasureType m : MeasureType.values()){
+                switch (m){
+                    case SCHWARTZBERG_COMPACTNESS:
+                        hashmap.put(m, (Math.random() * (schwartzbergCompactnessMax-schwartzbergCompactnessMin)) + schwartzbergCompactnessMin);
+                        break;
+                    case REOCK_COMPACTNESS:
+                        hashmap.put(m, (Math.random() * (reackCompactnessMax-reackCompactnessMin)) + reackCompactnessMin);
+                        break;
+                    case POLSBY_POPPER_COMPACTNESS:
+                        hashmap.put(m, (Math.random() * (polsbyCompactnessMax-polsbyCompactnessMin)) + polsbyCompactnessMin);
+                        break;
+                    case CONVEX_HULL_COMPACTNESS:
+                        hashmap.put(m, (Math.random() * (convexCompactnessMax-convexCompactnessMin)) + convexCompactnessMin);
+                        break;
+                    case GRAPH_COMPACTNESS:
+                        hashmap.put(m, (Math.random() * (graphCompactnessMax-graphCompactnessMin)) + graphCompactnessMin);
+                        break;
+                    case EFFICIENCY_GAP:
+                        hashmap.put(m, (Math.random() * (efficiencyGapMax-efficiencyGapMin)) + efficiencyGapMin);
+                        break;
+                    case POPULATION_EQUALITY:
+                        hashmap.put(m, (Math.random() * (populationEqualityMax-populationEqualityMin)) + populationEqualityMin);
+                        break;
+                }
+            }
+            preferences.add(new Preference(hashmap,maxMajMinDistricts,minMajMinDistricts,numDistricts,false, stateName));
+        }
+        return preferences;
+    }
+
+    protected BatchedPreferencesDAO(){
+
+    }
+
+    public double getSchwartzbergCompactnessMax() {
+        return schwartzbergCompactnessMax;
+    }
+
+    public void setSchwartzbergCompactnessMax(double schwartzbergCompactnessMax) {
+        this.schwartzbergCompactnessMax = schwartzbergCompactnessMax;
+    }
+
+    public double getSchwartzbergCompactnessMin() {
+        return schwartzbergCompactnessMin;
+    }
+
+    public void setSchwartzbergCompactnessMin(double schwartzbergCompactnessMin) {
+        this.schwartzbergCompactnessMin = schwartzbergCompactnessMin;
+    }
+
+    public double getReackCompactnessMax() {
+        return reackCompactnessMax;
+    }
+
+    public void setReackCompactnessMax(double reackCompactnessMax) {
+        this.reackCompactnessMax = reackCompactnessMax;
+    }
+
+    public double getReackCompactnessMin() {
+        return reackCompactnessMin;
+    }
+
+    public void setReackCompactnessMin(double reackCompactnessMin) {
+        this.reackCompactnessMin = reackCompactnessMin;
+    }
+
+    public double getPopulationEqualityMax() {
+        return populationEqualityMax;
+    }
+
+    public void setPopulationEqualityMax(double populationEqualityMax) {
+        this.populationEqualityMax = populationEqualityMax;
+    }
+
+    public double getPopulationEqualityMin() {
+        return populationEqualityMin;
+    }
+
+    public void setPopulationEqualityMin(double populationEqualityMin) {
+        this.populationEqualityMin = populationEqualityMin;
+    }
+
+    public double getPolsbyCompactnessMax() {
+        return polsbyCompactnessMax;
+    }
+
+    public void setPolsbyCompactnessMax(double polsbyCompactnessMax) {
+        this.polsbyCompactnessMax = polsbyCompactnessMax;
+    }
+
+    public double getPolsbyCompactnessMin() {
+        return polsbyCompactnessMin;
+    }
+
+    public void setPolsbyCompactnessMin(double polsbyCompactnessMin) {
+        this.polsbyCompactnessMin = polsbyCompactnessMin;
+    }
+
+    public double getConvexCompactnessMax() {
+        return convexCompactnessMax;
+    }
+
+    public void setConvexCompactnessMax(double convexCompactnessMax) {
+        this.convexCompactnessMax = convexCompactnessMax;
+    }
+
+    public double getConvexCompactnessMin() {
+        return convexCompactnessMin;
+    }
+
+    public void setConvexCompactnessMin(double convexCompactnessMin) {
+        this.convexCompactnessMin = convexCompactnessMin;
+    }
+
+    public double getGraphCompactnessMax() {
+        return graphCompactnessMax;
+    }
+
+    public void setGraphCompactnessMax(double graphCompactnessMax) {
+        this.graphCompactnessMax = graphCompactnessMax;
+    }
+
+    public double getGraphCompactnessMin() {
+        return graphCompactnessMin;
+    }
+
+    public void setGraphCompactnessMin(double graphCompactnessMin) {
+        this.graphCompactnessMin = graphCompactnessMin;
+    }
+
+    public double getEfficiencyGapMax() {
+        return efficiencyGapMax;
+    }
+
+    public void setEfficiencyGapMax(double efficiencyGapMax) {
+        this.efficiencyGapMax = efficiencyGapMax;
+    }
+
+    public double getEfficiencyGapMin() {
+        return efficiencyGapMin;
+    }
+
+    public void setEfficiencyGapMin(double efficiencyGapMin) {
+        this.efficiencyGapMin = efficiencyGapMin;
+    }
+
+    public boolean isGraphPartUpdate() {
+        return graphPartUpdate;
+    }
+
+    public void setGraphPartUpdate(boolean graphPartUpdate) {
+        this.graphPartUpdate = graphPartUpdate;
+    }
+
+    public int getMaxMajMinDistricts() {
+        return maxMajMinDistricts;
+    }
+
+    public void setMaxMajMinDistricts(int maxMajMinDistricts) {
+        this.maxMajMinDistricts = maxMajMinDistricts;
+    }
+
+    public int getMinMajMinDistricts() {
+        return minMajMinDistricts;
+    }
+
+    public void setMinMajMinDistricts(int minMajMinDistricts) {
+        this.minMajMinDistricts = minMajMinDistricts;
+    }
+
+    public int getNumDistricts() {
+        return numDistricts;
+    }
+
+    public void setNumDistricts(int numDistricts) {
+        this.numDistricts = numDistricts;
+    }
+
+    public String getStateName() {
+        return stateName;
+    }
+
+    public void setStateName(String stateName) {
+        this.stateName = stateName;
+    }
+}
